fef29d014d2b30e11c714166d1c8c65c15c2cdd6
richardembatista-cmyk <richard.embatista@gmail.com>
2026-02-16 00:07:03 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log('Bot está 
+    client.login(process.env.TOKEN);
